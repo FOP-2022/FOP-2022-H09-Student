2996d284bd9e832fdbe996654f5d9052e881a815
Nyanyan <46193012+zentox@users.noreply.github.com>
2021-12-31 19:30:50 +0100

    Add example for H1.1

--- src/main/java/h09/Main.java
+++ src/main/java/h09/Main.java
@@ -2,6 +2,11 @@ package h09;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+//    final Traits<Number, Number, Number> traits = new Traits<Number, Number, Number>(
+//      Integer.class::isInstance,
+//      Object::hashCode,
+//      (number, o) -> number.intValue() * o.hashCode(),
+//      12.2
+//    );
   }
 }
